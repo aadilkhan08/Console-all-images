ce6ca08871a2546fefa4d4a7480c9837f1e3679c
Aadil <aadilmoid@gmail.com>
2023-04-03 00:04:32 +0530

    last commit

--- download.js
+++ download.js
@@ -1,6 +1,5 @@
 var imageUrls = [];
 har.log.entries.forEach(function (entry) {
-  // This step will filter out all URLs except images. If you just want e.g. just jpg's then check mimeType against "image/jpeg", etc.
   if (entry.response.content.mimeType.indexOf("image/") !== 0) return;
   imageUrls.push(entry.request.url);
 });
